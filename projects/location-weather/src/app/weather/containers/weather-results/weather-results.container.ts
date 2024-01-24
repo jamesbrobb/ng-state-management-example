3c757aa270435170818a2dbcc5e808787fefe213
jamesbrobb <jamesbenrobb@gmail.com>
2024-01-24 14:51:18 +0000

    added visual loader for weather data

--- projects/location-weather/src/app/weather/containers/weather-results/weather-results.container.ts
+++ projects/location-weather/src/app/weather/containers/weather-results/weather-results.container.ts
@@ -1,15 +1,16 @@
 import {Component, inject, Input} from '@angular/core';
-import {AsyncPipe} from "@angular/common";
+import {AsyncPipe, NgIf} from "@angular/common";
 import {WeatherResultsComponent} from "@jbr/shared";
 import {LOCATION_FACADE, DATE_FACADE, WEATHER_FACADE} from "@jbr/state/shared";
 import {combineLatest, of, switchMap} from "rxjs";
 import {DatePickerContainer} from "../../../date/containers/date-picker/date-picker.container";
 import {MatDividerModule} from "@angular/material/divider";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 
 @Component({
   selector: 'weather-results-container',
   standalone: true,
-  imports: [WeatherResultsComponent, AsyncPipe, WeatherResultsComponent, DatePickerContainer, MatDividerModule],
+  imports: [WeatherResultsComponent, AsyncPipe, WeatherResultsComponent, DatePickerContainer, MatDividerModule, NgIf, MatProgressSpinnerModule],
   templateUrl: './weather-results.container.html',
   styleUrls: ['./weather-results.container.scss']
 })
